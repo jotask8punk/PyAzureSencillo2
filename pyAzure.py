8416ce2fcabb6f6dc0612bf2005d321b51bcafb4
jotask8punk <skaterpro01@hotmail.com>
2022-08-15 10:18:37 -0500

    first commit

---
+++ pyAzure.py
@@ -0,0 +1,11 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return Response("Hola", status=200)    
+
+
+if __name__ == '__main__':
+    app.run()
